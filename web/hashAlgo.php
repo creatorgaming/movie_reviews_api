512d7a0dde02be64bf2b4398c8111076e4bb4389
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-15 23:14:58 +0530

    login.php Updated, sighup.php Added, connect.php Added, hash_algo.php Added
    Basic funcctionality for hash code and login is done.

---
+++ web/hashAlgo.php
@@ -0,0 +1,9 @@
+<?php
+$data = "hello";
+    foreach (hash_algos() as $v) { 
+        $r = hash($v, $data, false); 
+        printf("%-12s %3d %s\n", $v, strlen($r), $r);
+        echo "<br>"; 
+}
+
+?>    
--- web/index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    $data = file_get_contents('php://input');
-    $decoded = json_decode($data,false);
-    echo $decoded->demo;
-?>
