60903f725ce2e51e085b3cdcdc063a0a6ce72176
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-17 14:16:36 +0530

    Movies.php Added

--- web/connect.php
+++ web/connect.php
@@ -1,21 +1,13 @@
 <?php 
-<<<<<<< HEAD
-    $servername = "sql12.freesqldatabase.com";
-    $username = "sql12233034";
-    $pass = "bSsMvMeKgB";
-    $database = "sql12233034";   
-    // $servername = "localhost";
-    // $username = "root";
-    // $pass = "";
-    // $database = "radiant-dusk-89811";
-    $conn = mysqli_connect($servername,$username,$pass,$database);
-=======
+    // $servername = "sql12.freesqldatabase.com";
+    // $username = "sql12233034";
+    // $pass = "bSsMvMeKgB";
+    // $database = "sql12233034";   
     $servername = "localhost";
     $username = "root";
-    $password = "";
-    $database = "radiant-dusk-89811";   
-    $conn = mysqli_connect($servername,$username,$password,$database);
->>>>>>> heroku/master
+    $pass = "";
+    $database = "radiant-dusk-89811";
+    $conn = mysqli_connect($servername,$username,$pass,$database);
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
--- web/signup.php
+++ web/signup.php
@@ -1,66 +1,27 @@
-<<<<<<< HEAD
 <?php  
     include 'connect.php';
     
-=======
-<?php
-    include 'connect.php';
-
->>>>>>> heroku/master
     $data = file_get_contents('php://input');
     $decoded = json_decode($data);
     $email = $decoded->email;
     $password = $decoded->password;
     $name = $decoded->name;
-    $hashCode;
-<<<<<<< HEAD
-    $hashCodeString;
-=======
->>>>>>> heroku/master
 
     //  Find if the email is valid or not
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        GLOBAL $hashCode;
-        $hashCode = NULL;
+        echo json_encode("NULL");
     }else {
-        GLOBAl $hashCode;
-<<<<<<< HEAD
-        GLOBAL $hashCodeString;
         $duplicateCheckQuery = "SELECT * FROM userLoginInfo 
                                 WHERE email = '$email' ";
-        $query = mysqli_query($conn,$duplicateCheckQuery);
-        if(!$query){
-            echo "ERROR";
-        }
-        $duplicateCheckQueryResult = mysqli_fetch_assoc($query);
+        $duplicateCheckQueryResult = mysqli_fetch_assoc(mysqli_query($conn,
+                                                        $duplicateCheckQuery));
         if(!$duplicateCheckQueryResult){
-            $hashCodeString = $name.$password;
-            $hashCode = md5($hashCodeString);
-            $query = "INSERT INTO userLoginInfo (`name`, `email`, `password`, `hash`) 
-                        VALUES ('$name','$email','$password',MD5('$hashCodeString'))";
+            $query = "INSERT INTO userLoginInfo (`name`, `email`, `password`, `sid`) 
+                        VALUES ('$name','$email','$password',)";
             mysqli_query($conn, $query);
         }else{
             echo "Duplicate Email";
         }
     }
-    echo json_encode($hashCode);
-=======
-        $duplicateCheckQuery = "SELECT * FROM userlogininfo WHERE email = '$email' ";
-        $duplicateCheckQueryResult = mysqli_fetch_assoc(mysqli_query($conn,$duplicateCheckQuery));
-        if(!$duplicateCheckQueryResult){
-            $hashCode = hash('md5',$name,$password);
-            $query = "INSERT INTO userlogininfo (`name`, `email`, `password`, `hash`) VALUES ('$name','$email','$password','$hashCode')";
-            mysqli_query($conn, $query);
-            // if(mysqli_query($conn, $query)){
-    		// 	echo "query saved";
-	    	// }else{
-    		// 	echo "query not saved";
-	    	// }
-            //echo "<br>".$hashCode."<br>";
-        }else{
-            echo json_encode("duplicate e-mail");
-        }
-    }
-    echo "HASH CODE: ".json_encode($hashCode);
->>>>>>> heroku/master
+    echo json_encode();
 ?>
