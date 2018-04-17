fa15cb6642b42044402e59a2c5049baf12affbd1
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-17 16:15:31 +0530

    Movies.php Updated

--- web/connect.php
+++ web/connect.php
@@ -1,12 +1,12 @@
 <?php 
-    // $servername = "sql12.freesqldatabase.com";
-    // $username = "sql12233034";
-    // $pass = "bSsMvMeKgB";
-    // $database = "sql12233034";   
-    $servername = "localhost";
-    $username = "root";
-    $pass = "";
-    $database = "radiant-dusk-89811";
+    $servername = "sql12.freesqldatabase.com";
+    $username = "sql12233034";
+    $pass = "bSsMvMeKgB";
+    $database = "sql12233034";   
+    // $servername = "localhost";
+    // $username = "root";
+    // $pass = "";
+    // $database = "radiant-dusk-89811";
     $conn = mysqli_connect($servername,$username,$pass,$database);
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
