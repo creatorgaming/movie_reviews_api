d784b222a2a72e3b5b7e7840f2e02406b35c7acf
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-18 21:05:54 +0530

    insertComment.php Updated

--- web/insertComment.php
+++ web/insertComment.php
@@ -1,16 +1,17 @@
 <?php
     $data = file_get_contents('php://input');
     $decoded = json_decode($data);
+    $time = date("h:i:s", time());
+    echo $time;
+    // if($decoded){
+    //     include "connect.php";
+    //     $jwt = $decoded->jwt;
+    //     $comment = $decoded->comment;
+    //     $movieId = $decoded->id;
 
-    if($decoded){
-        include "connect.php";
-        $jwt = $decoded->jwt;
-        $comment = $decoded->comment;
-        $movieId = $decoded->id;
+    //     $insertQuery = "INSERT INTO `usercomments`(`movieId`, `comment`, `jwt`) VALUES ('$movieId','$comment','$jwt')";
+    //     $insertQueryRun = mysqli_query($conn,$insertQuery);        
+    // }
 
-        $insertQuery = "INSERT INTO `usercomments`(`movieId`, `comment`, `jwt`) VALUES ('$movieId','$comment','$jwt')";
-        $insertQueryRun = mysqli_query($conn,$insertQuery);        
-    }
-
-    include "disconnect.php"
+    // include "disconnect.php"
 ?>
