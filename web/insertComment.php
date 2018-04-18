1afaab08e7527c624482665f35c94e5505d8edcd
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-18 21:10:28 +0530

    insertComment.php Updated

--- web/insertComment.php
+++ web/insertComment.php
@@ -12,19 +12,4 @@
     //     $insertQuery = "INSERT INTO `usercomments`(`movieId`, `comment`, `jwt`) VALUES ('$movieId','$comment','$jwt')";
     //     $insertQueryRun = mysqli_query($conn,$insertQuery);        
     // }
-
-<<<<<<< HEAD
-    // include "disconnect.php"
 ?>
-=======
-        $insertQuery = "INSERT INTO `usercomments`(`movieId`, `comment`, `jwt`) VALUES ('$movieId','$comment','$jwt')";
-        $insertQueryRun = mysqli_query($conn,$insertQuery);        
-    }
-<<<<<<< HEAD
-?>
-=======
-
-    include "disconnect.php"
-?>
->>>>>>> 68674e3a6a58f26670f9bb6f7a8dfc59fe610bb8
->>>>>>> 8dc5d6d1cbfa2b8e55b6b60ecaf2676a59cb42ba
