15c4bb19db694b53fa3937c810c036f2d88f4821
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-18 19:53:24 +0530

    dataHandler.php Updated

--- web/dataHandler.php
+++ web/dataHandler.php
@@ -10,14 +10,14 @@
 	}
 	function add_comment($movieId,$userEmail,$comment,$commentConn,$comment_db)
 	{
-		$sql = 'INSERT INTO  `$comment_db` (MovieID, UserEmail, Comment) VALUES ('$movieId','$userEmail','$comment')';
+		$sql = "INSERT INTO  `$comment_db` (MovieID, UserEmail, Comment) VALUES ('$movieId','$userEmail','$comment')";
 		$result = mysqli_query($commentconn,$sql);
 		if(!$result)
 			return -1;
 	}
 	function search_review($movieId,$reviewConn,$movieReview_db)
 	{
-		$sql = 'SELECT * FROM `$movieReview_db` WHERE MovieID = '$movieId'';
+		$sql = "SELECT * FROM `$movieReview_db` WHERE MovieID = '$movieId'";
 		$result = mysqli_query($commentconn,$sql);
 		if($result)
 			return "true";
@@ -26,7 +26,7 @@
 	}
 	function add_review($movieId,$review,$description,$reviewConn,$movieReview_db)
 	{
-		$sql = 'INSERT INTO  `$movieReview_db` (MovieID, Description, Review) VALUES ('$movieId','$description','$review')';
+		$sql = "INSERT INTO  `$movieReview_db` (MovieID, Description, Review) VALUES ('$movieId','$description','$review')";
 		$result = mysqli_query($reviewConn,$sql);
 		if(!$result)
 			return -1;
