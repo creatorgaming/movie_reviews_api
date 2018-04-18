8fd8d8fc149519e49e67f76e01afc6ee5a4ae045
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-18 21:44:12 +0530

    displayComment.php Updated

--- web/insertComment.php
+++ web/insertComment.php
@@ -1,15 +1,15 @@
 <?php
     $data = file_get_contents('php://input');
     $decoded = json_decode($data);
-    $time = date("d/m/y", time());
-    echo $time;
-    // if($decoded){
-    //     include "connect.php";
-    //     $jwt = $decoded->jwt;
-    //     $comment = $decoded->comment;
-    //     $movieId = $decoded->id;
+    
+    if($decoded){
+        include "connect.php";
+        $date = date("d/m/y", time());
+        $jwt = $decoded->jwt;
+        $comment = $decoded->comment;
+        $movieId = $decoded->id;
 
-    //     $insertQuery = "INSERT INTO `usercomments`(`movieId`, `comment`, `jwt`) VALUES ('$movieId','$comment','$jwt')";
-    //     $insertQueryRun = mysqli_query($conn,$insertQuery);        
-    // }
+        $insertQuery = "INSERT INTO `usercomments`(`movieId`, `comment`, 'date', `jwt`) VALUES ('$movieId','$comment','$date','$jwt')";
+        $insertQueryRun = mysqli_query($conn,$insertQuery);        
+    }
 ?>
