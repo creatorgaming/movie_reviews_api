b1425c03cd2677651f4744bb4ec1d9053c31b12a
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-19 13:20:53 +0530

    App uploaded 3

--- web/desc.php
+++ web/desc.php
@@ -4,14 +4,15 @@
     $data = file_get_contents('php://input');
     $decoded = json_decode($data);
     $movieId = $decoded->id;
-    $description = NULL;
+    $description = array();
     if($movieId){
-        $query = "SELECT description FROM movieinfo WHERE
+        $query = "SELECT description,rating FROM movieinfo WHERE
                     movieId = '$movieId' ";
         $queryRun = mysqli_query($conn,$query);
         $queryResult = mysqli_fetch_assoc($queryRun);
         if($queryResult){
-            $description = $queryResult['description'];
+            $description[0] = $queryResult['description'];
+            $description[1] = $queryResult['rating'];
         }
     }
 
