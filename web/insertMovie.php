9ca8565aad549ebdc7cd4ab473adfe08ef9fe4b6
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-19 17:19:53 +0530

    insertMovie.php Added, decision.php Updated

---
+++ web/insertMovie.php
@@ -0,0 +1,35 @@
+<?php
+    include 'connect.php';
+
+    $data = file_get_contents('php://input');
+    $decoded = json_decode($data);
+    
+    if($decoded){
+        $descprition = $decoded->description;
+        $movieId = $decoded->id;
+        $review = $decoded->review;
+        $rating = $decoded->rating;
+
+        $duplicateCheck = "SELECT * FROM movieinfo WHERE
+                                 movieId = '$movieId'";
+        $duplicateCheckQueryRun = mysqli_query($conn,$duplicateCheck);
+        $duplicateCheckQueryResult = mysqli_fetch_assoc($duplicateCheckQueryRun);
+        if(!$duplicateCheckQueryResult){
+            $insertQuery = "INSERT INTO `movieinfo`(movieId, description, review, rating) VALUES ('$movieId','$descprition','$review','$rating')";
+            
+            $insertQueryRun = mysqli_query($conn, $insertQuery);
+
+            if($insertQueryRun){
+                echo json_encode(1);
+            }else {
+                echo json_encode(NULL);
+            }
+            
+        }else {
+            echo json_encode(NULL);
+        }
+    }else {
+        echo json_encode(NULL);
+    }
+        include "disconnect.php";
+?>
