99aa7b288c9426b42ee7957da6c0a47ddd90ca15
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-04-20 12:26:45 +0530

    desc.php Updated,displayComment.php Updated

--- web/displayComment.php
+++ web/displayComment.php
@@ -1,20 +1,17 @@
 <?php
-    
     $movieId = $_GET['id'];
-
     if($movieId){
         include "connect.php";
         include "commentClass.php";
-        $comments = array();
 
+        $comments = array();
         $fetchQuery = "SELECT jwt,comment,date FROM userComments WHERE movieId = '$movieId'";
         $fetchQueryRun = mysqli_query($conn,$fetchQuery);
-        
         while($result = mysqli_fetch_array($fetchQueryRun)){
             $jwt = $result['jwt'];
             $comment = $result['comment'];
             $date = $result['date'];
-            $commentsEntry = new commentClass($jwt,$comment,$date);            
+            $commentsEntry = new commentClass($jwt,$comment,$date);
             $comments[] = $commentsEntry;
         }  
          
--- web/review.php
+++ web/review.php
@@ -1,17 +1,19 @@
 <?php
-    include "connect.php";
+    
     
     $movieId = $_GET['id'];
     $review = NULL;
     if($movieId){
+        include "connect.php";
         $query = "SELECT review FROM movieinfo WHERE movieId = '$movieId' ";
         $queryRun = mysqli_query($conn,$query);
         $queryResult = mysqli_fetch_assoc($queryRun);
+        print_r($queryResult);
         if($queryResult){
             $review = $queryResult['review'];
         }
     }
 
-    echo json_encode($review);
+    //echo json_encode($review);
     include "disconnect.php";
 ?>
